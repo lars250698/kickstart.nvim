de15d49273e10d15f29570fc2d5ee818828563e9
Lars Eppinger <36228556+lars250698@users.noreply.github.com>
2025-06-11 00:32:32 +0200

    remove multicursor, remove startup screen, replace autosession with persistence.nvim

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,10 +1,37 @@
 return {
   {
-    "rmagatti/auto-session",
-    lazy = false,
-
-    opts = {
-      suppressed_dirs = { "~/", "~/Downloads", "/" },
+    'folke/persistence.nvim',
+    event = 'BufReadPre',
+    opts = {},
+    keys = {
+      {
+        '<leader>qs',
+        function()
+          require('persistence').load()
+        end,
+        desc = 'Restore Session',
+      },
+      {
+        '<leader>qS',
+        function()
+          require('persistence').select()
+        end,
+        desc = 'Select Session',
+      },
+      {
+        '<leader>ql',
+        function()
+          require('persistence').load { last = true }
+        end,
+        desc = 'Restore Last Session',
+      },
+      {
+        '<leader>qd',
+        function()
+          require('persistence').stop()
+        end,
+        desc = "Don't Save Current Session",
+      },
     },
   },
 }
